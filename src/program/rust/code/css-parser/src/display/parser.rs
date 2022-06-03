58adac8dd61f3d7a1936524552157743458ac0ca
imxood <imxood@gmail.com>
2022-06-03 13:12:11 +0800

    .

--- src/program/rust/code/css-parser/src/display/parser.rs
+++ src/program/rust/code/css-parser/src/display/parser.rs
@@ -1,4 +1,8 @@
-use nom::sequence::preceded;
+use nom::bytes::complete::tag;
+use nom::character::complete::multispace0;
+use nom::combinator::map;
+use nom::multi::many0;
+use nom::sequence::{delimited, preceded, tuple};
 use nom::IResult;
 use nom::{branch::alt, bytes::complete::take_till1};
 
@@ -10,86 +14,198 @@ use super::flex::FlexDirection;
 
 // }
 
-// grid: <'grid-template'> | <'grid-template-rows'> / [ auto-flow && dense? ] <'grid-auto-columns'>? | [ auto-flow && dense? ] <'grid-auto-rows'>? / <'grid-template-columns'>
-// grid-template: none | [ <'grid-template-rows'> / <'grid-template-columns'> ] | [ <line-names>? <string> <track-size>? <line-names>? ]+ [ / <explicit-track-list> ]?
-// grid-template-rows:
+// /* <'grid-template'> values */
+// grid: none;
+// grid: "a" 100px "b" 1fr;
+// grid: [linename1] "a" 100px [linename2];
+// grid: "a" 200px "b" min-content;
+// grid: "a" minmax(100px, max-content) "b" 20%;
+// grid: 100px / 200px;
+// grid: minmax(400px, min-content) / repeat(auto-fill, 50px);
+
+// /* <'grid-template-rows'> /
+//    [ auto-flow && dense? ] <'grid-auto-columns'>? values */
+// grid: 200px / auto-flow;
+// grid: 30% / auto-flow dense;
+// grid: repeat(3, [line1 line2 line3] 200px) / auto-flow 300px;
+// grid: [line1] minmax(20em, max-content) / auto-flow dense 40%;
 
 // /* [ auto-flow && dense? ] <'grid-auto-rows'>? /
 //    <'grid-template-columns'> values */
-//    grid: auto-flow / 200px;
-//    grid: auto-flow dense / 30%;
-//    grid: auto-flow 300px / repeat(3, [line1 line2 line3] 200px);
-//    grid: auto-flow dense 40% / [line1] minmax(20em, max-content);
+// grid: auto-flow / 200px;
+// grid: auto-flow dense / 30%;
+// grid: auto-flow 300px / repeat(3, [line1 line2 line3] 200px);
+// grid: auto-flow dense 40% / [line1] minmax(20em, max-content);
 
-// 优先级: 数量符号 > 组合 [] > 并置 "多个空格" > 与 "&&" > 或 "||" > 互斥 "|"
+// /* Global values */
+// grid: inherit;
+// grid: initial;
+// grid: unset;
 
-// 解析过程是: 1. 解析优先级 遍历每一个优先级, 用每一个优先级去 目标中 查询 是否存在匹配, 若匹配到了, 则解析, 对应优先级下的数据,
+// grid: [ auto-flow && dense? ] <grid-auto-rows>? / <grid-template-columns> | <grid-template> | <grid-template-rows> / [ auto-flow && dense? ] <grid-auto-columns>?
 
+// 优先级: 数量符号 > 组合 [] > 与 "&&" > 或 "||" > 互斥 "|"
+
+#[derive(Debug)]
 pub enum RuleObject {
-    // 方括号, 表示 多个规则的集合
+    // 方括号, 表示 多个规则的组合
     // bold [ thin && <length> ]
-    Brackets,
-    /// 并置, 一个或多个空格, 必须按照特定的顺序放置
-    /// 如: bold <length> , thin 可以是: bold 100px , thin, 顺序不能颠倒
-    Juxtaposition,
-    // 各个部分必须出现, 但是顺序任意
+    Brackets(Box<RuleObject>, Option<QuantitySymbol>),
+
+    // 双引号, 各个选项必须出现, 但是顺序任意
     // 如: bold && <length>:
-    DoubleAmpersand,
+    DoubleAmpersand(Vec<Box<RuleObject>>),
+
     // 双杠, 至少一个 最多不超过 双杠参数值 的数量, 顺序任意
     // 如: A || B || C， 可以是 A, A B, B C A 等
-    DoubleBar(Vec<RuleObject>),
+    DoubleBar(Vec<Box<RuleObject>>),
+
     // 单杠, 只能取 这些选项中的一个
     // 如: A | B | C， 只能是 A 或 B 或 C
-    SingleBar(Vec<RuleObject>),
-    // 可变的值
-    Variable(Variable),
-    // auto-flow
-    Symbol(String),
-    // [ auto-flow && dense? ]
-    Combinator(Vec<RuleObject>),
+    SingleBar(Vec<Box<RuleObject>>),
+
+    // 表示 一个特定的属性类型 的值
+    Variable(String, Option<QuantitySymbol>),
+
+    // 符号, 如: auto-flow
+    Symbol(String, Option<QuantitySymbol>),
 }
-// <'grid-template'> | <'grid-template-rows'> / [ auto-flow && dense? ] <'grid-auto-columns'>?
+
+#[derive(Debug)]
+pub enum QuantitySymbol {
+    // ? 0次或1次
+    QuestionMark,
+    // * 0次或多次
+    Asterisk,
+    // + 1次或多次
+    Plus,
+    // {m, n} 至少m次 最多n次
+    CurlyBraces(u8, u8),
+    // #  一次或多次, 与 + 功能一致, 但是多次出现必须以逗号分隔
+    // 如: bold smaller#, 例子: bold smaller, smaller, smaller
+    HashMark,
+    // []! 方括号后面的叹号表示该组是必须的, 并且至少产生一个值, 即使组内项目全部允许忽略, 也至少保留一个
+    ExclamationPoint,
+}
+
+// impl Parser for QuantitySymbol {
+//     fn parse(i: &str) -> IResult<&str, Self> {
+//         preceded(multispace0, second)(i)
+//     }
+// }
+
+// <grid-template> | <grid-template-rows> / [ auto-flow && dense? ] <grid-auto-columns>?
 impl Parser for RuleObject {
     fn parse(i: &str) -> IResult<&str, Self> {
-        alt(())
+        alt((
+            parse_brackets,
+            parse_double_ampersand,
+            parse_double_bar,
+            parse_single_bar,
+            parse_variable,
+            parse_string,
+        ))(i)
     }
 }
 
-// 解析方括号
+// pub fn parse_rule_object(i: &str) -> IResult<&str, RuleObject> {
+//     let (i, obj) =
+// }
+
+// 解析 方括号
 fn parse_brackets(i: &str) -> IResult<&str, RuleObject> {
+    delimited(
+        multispace0,
+        map(
+            delimited(nom_char('['), RuleObject::parse, nom_char(']')),
+            |obj| RuleObject::Brackets(Box::new(obj), None),
+        ),
+        multispace0,
+    )(i)
+}
+
+pub fn parse_operation(i: &str) -> IResult<&str, RuleObject> {
+    alt((parse_brackets, parse_variable, parse_string))(i)
+    // parse_double_ampersand,
+    // parse_double_bar,
+    // parse_single_bar,
+    // parse_variable,
+    // parse_string,
+}
+
+// 解析 双引号
+fn parse_double_ampersand(i: &str) -> IResult<&str, RuleObject> {
+    let (i, obj1) = alt((parse_brackets, parse_variable, parse_string))(i)?;
+
+    let (i, obj2) = delimited(
+        multispace0,
+        many0(preceded(
+            tag("&&"),
+            // 解析优先级更高的选项, 若失败, 则自动降级 解析, 直到
+            parse_double_ampersand,
+        )),
+        multispace0,
+    )(i)?;
+
+    let mut objs = vec![Box::new(obj1)];
+    for obj in obj2 {
+        objs.push(Box::new(obj));
+    }
+
+    Ok((i, RuleObject::DoubleAmpersand(objs)))
+}
+
+// 解析 双杠
+// A || B || C || D ... E ... F
+fn parse_double_bar(i: &str) -> IResult<&str, RuleObject> {
     let i = i.trim_start();
     let (i, input) = preceded(nom_char('['), skip_sp(take_till1(|c| c == ']')))(i)?;
     RuleObject::parse(i)
 }
 
-// 解析并置, 一个或多个括号
-fn parse_juxtaposition(i: &str) -> IResult<&str, RuleObject> {
+// 解析 单杠
+fn parse_single_bar(i: &str) -> IResult<&str, RuleObject> {
     let i = i.trim_start();
     let (i, input) = preceded(nom_char('['), skip_sp(take_till1(|c| c == ']')))(i)?;
     RuleObject::parse(i)
 }
 
-pub enum Variable {
-    // 有尖括号 不带单引号 的类型
-    // 如: <track-size>
-    Type,
-    // 有尖括号 且带 单引号, 表示 一个特定的属性类型 的值
-    // 如: <'grid-template'>
-    PropType,
+// 解析 可变的值
+fn parse_variable(i: &str) -> IResult<&str, RuleObject> {
+    // e.g., <background_color>
+    delimited(
+        multispace0,
+        map(
+            delimited(
+                nom_char('<'),
+                take_till1(|c: char| !c.is_alphanumeric() || c != '_'),
+                nom_char('>'),
+            ),
+            |i: &str| RuleObject::Variable(i.to_string(), None),
+        ),
+        multispace0,
+    )(i)
 }
 
-pub enum QuantitySymbol {
-    // ? 0次或1次
-    QuestionMark(Option<RuleObject>),
-    // * 0次或多次
-    Asterisk(Vec<RuleObject>),
-    // + 1次或多次
-    Plus(Vec<RuleObject>),
-    // {m, n} 至少m次 最多n次
-    CurlyBraces(Vec<RuleObject>),
-    // #  一次或多次, 与 + 功能一致, 但是多次出现必须以逗号分隔
-    // 如: bold smaller#, 例子: bold smaller, smaller, smaller
-    HashMark(Vec<RuleObject>),
-    // []! 方括号后面的叹号表示该组是必须的, 并且至少产生一个值, 即使组内项目全部允许忽略, 也至少保留一个
-    ExclamationPoint(Vec<RuleObject>),
+// 解析 符号
+// 字母 数字 和 下划线 组成的字符串
+fn parse_string(i: &str) -> IResult<&str, RuleObject> {
+    // e.g., Bold
+    delimited(
+        multispace0,
+        map(
+            take_till1(|c: char| !c.is_alphanumeric() && c != '_'),
+            |i: &str| RuleObject::Variable(i.to_string(), None),
+        ),
+        multispace0,
+    )(i)
+}
+
+mod test {
+    use super::parse_double_ampersand;
+
+    #[test]
+    fn test_parse_double_ampersand() {
+        println!("{:#?}", parse_double_ampersand("A && B && C && D").unwrap());
+    }
 }
--- src/program/rust/code/css-parser/src/main.rs
+++ src/program/rust/code/css-parser/src/main.rs
@@ -1,377 +1,175 @@
-use core::fmt::{Display, Formatter};
+use std::{collections::HashMap, process::Command};
 
-use nom::branch::alt;
-use nom::character::complete::none_of;
-use nom::combinator::{all_consuming, map, peek};
-use nom::multi::{many0, separated_list1};
-use nom::{
-    bytes::complete::{is_not, tag},
-    sequence::{delimited, preceded, tuple},
-};
-use nom::{IResult, Parser as NomParser};
+use convert_case::{Case, Casing};
+use serde_json::{json, Value};
+use simplelog::*;
+use tera::{Context, Result, Tera};
 
-use parse::{skip_sp, Parser};
-use properties::Property;
-
-mod display;
 mod color;
+mod display;
 mod parse;
 mod properties;
+// mod selectors;
 mod serialize;
 mod types;
 
-// #[path = "./def_properties.rs"]
-// pub mod properties;
+fn build_properties() {
+    let current_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    let in_file = trace_file(current_dir.clone() + "/src/hbs/properties.tera");
+    let out_file = current_dir.clone() + "/src/properties.rs";
+
+    let data = json!({
+        "properties": {
+            "width": "Float",
+            "height": "Float",
+
+            "background": {
+                "background-color": "Color",
+                "background-position-x": "Float",
+                "background-position-y": "Float",
+            },
+
+            // "display": {
+            //     "flex": [
+            //         "flex-direction",
+            //         "flex-wrap",
+            //         "flex-flow",
+            //         "align-items",
+            //         "align-content",
+            //         "justify-content",
+            //     ],
+            //     "flex-item": [
+            //         "flex",
+            //         "flex-gow",
+            //         "flex-shrink",
+            //         "flex-basis",
+            //         "align-self",
+            //         "order",
+            //     ],
+            //     "grid": [
+            //         /* grid-gap: <grid-row-gap> <grid-column-gap>; */
+            //         {
+            //             "grid-gap": [
+            //                 "grid-row-gap",
+            //                 "grid-column-gap",
+            //             ]
+            //         },
+            //         /* place-items: <align-items> <justify-items>; */
+            //         {
+            //             "place-items": [
+            //                 "align-items",
+            //                 "justify-items",
+            //             ]
+            //         },
+            //         /* place-content: <align-content> <justify-content>; */
+            //         {
+            //             "place-content": [
+            //                 "align-content",
+            //                 "justify-content",
+            //             ]
+            //         },
+            //         /* grid-template: <grid-template-columns> <grid-template-rows> <grid-template-areas>; */
+            //         {
+            //             "grid-template": [
+            //                 "grid-template-columns",
+            //                 "grid-template-rows",
+            //                 "grid-template-areas",
+            //             ]
+            //         },
+            //         /* grid: <grid-template-rows> <grid-template-columns> <grid-template-areas> <grid-auto-rows> <grid-auto-columns> <grid-auto-flow>; */
+            //         {
+            //             "grid": [
+            //                 "grid-auto-rows",
+            //                 "grid-auto-columns",
+            //                 "grid-auto-flow",
+            //             ]
+            //         }
+            //     ],
+            //     "grid-item": [
+            //         "flex",
+            //         "flex-gow",
+            //         "flex-shrink",
+            //         "flex-basis",
+            //         "align-self",
+            //         "order",
+            //     ],
+            // },
+        }
+    });
+
+    let context = Context::from_value(data).unwrap();
+
+    let mut tera = Tera::default();
+
+    tera.register_function("pascal", |args: &HashMap<String, Value>| -> Result<Value> {
+        let text = args.get("text").unwrap().as_str().unwrap();
+        Ok(text.to_case(Case::Pascal).into())
+    });
+    tera.register_function("snake", |args: &HashMap<String, Value>| -> Result<Value> {
+        let text = args.get("text").unwrap().as_str().unwrap();
+        Ok(text.to_case(Case::Snake).into())
+    });
+    tera.register_function("kebab", |args: &HashMap<String, Value>| -> Result<Value> {
+        let text = args.get("text").unwrap().as_str().unwrap();
+        Ok(text.to_case(Case::Kebab).into())
+    });
+
+    tera.add_template_file(&in_file, Some("properties"))
+        .unwrap();
+
+    tera.render_to(
+        "properties",
+        &context,
+        std::fs::File::create(&out_file).unwrap(),
+    )
+    .unwrap();
+
+    rustfmt_generated_file(&out_file);
+}
+
+fn rustfmt_generated_file(source_file: &str) {
+    let rustfmt_path = which::which("rustfmt")
+        .expect("cann't find rustfmt, please install \"rustup component add rustfmt\"");
+    Command::new(&*rustfmt_path)
+        .arg(source_file)
+        .output()
+        .expect(&format!(
+            "rustfmt format file failed, file: {}",
+            source_file
+        ));
+}
+
+fn trace_file(file: String) -> String {
+    println!("cargo:rerun-if-changed={}", &file);
+    file
+}
 
 fn main() {
-    let css_data = include_str!("./simple.css");
-    let (_, ret) = parse_css(css_data).unwrap();
-    println!("{:#?}", &ret);
+    log_init();
+
+    println!("cargo:rerun-if-changed=build.rs");
+
+    build_properties();
+
+    log::info!("build ok");
 }
 
-#[derive(Debug, PartialEq)]
-pub struct CssEntities(Vec<CssEntity>);
-
-#[derive(Debug, PartialEq)]
-pub enum CssEntity {
-    Block {
-        selectors: Selectors,
-        properties: Properties,
-    },
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Selectors(pub(crate) Vec<Selector>);
-
-#[derive(Debug, PartialEq)]
-pub struct Properties(pub(crate) Vec<Property>);
-
-#[derive(Debug, PartialEq)]
-pub enum Selector {
-    /// # 开头, 唯一Id匹配的元素
-    Id(String),
-    /// . 开头,
-    Class(String),
-    /// 纯字符串
-    Tag(String),
-    /// * 表示所有子元素
-    All,
-    /// 组合多种选择器
-    /// 如: "#user .friend" 表示 所有id为user的元素下, 所有的class为friend的元素
-    Combinator(Vec<Selector>),
-}
-
-impl Display for Selector {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Selector::Id(id) => {
-                write!(f, "#{}", id)
-            }
-            Selector::Class(class) => {
-                write!(f, ".{}", class)
-            }
-            Selector::Tag(tag) => {
-                write!(f, "{}", tag)
-            }
-            Selector::All => {
-                write!(f, "*")
-            }
-            Selector::Combinator(c) => {
-                if let Some((last, c)) = c.split_last() {
-                    for s in c.iter() {
-                        write!(f, "{} ", s)?;
-                    }
-                    write!(f, "{}", last)?;
-                }
-                Ok(())
-            }
-        }
-    }
-}
-
-pub fn parse_css(i: &str) -> IResult<&str, CssEntities> {
-    all_consuming(parse_entities)(i)
-}
-
-fn parse_entities(i: &str) -> IResult<&str, CssEntities> {
-    map(skip_sp(many0(parse_entity)), |e| CssEntities(e))(i)
-}
-
-fn parse_entity(i: &str) -> IResult<&str, CssEntity> {
-    parse_block(i)
-}
-
-/// 解析 Block
-fn parse_block(i: &str) -> IResult<&str, CssEntity> {
-    map(
-        tuple((skip_sp(parse_selectors), parameter_block(parse_parameters))),
-        |(selectors, properties)| CssEntity::Block {
-            selectors,
-            properties,
-        },
-    )(i)
-}
-
-/// 解析 选择器列表
-fn parse_selectors(i: &str) -> IResult<&str, Selectors> {
-    map(
-        is_not_block_ending(separated_list1(tag(","), skip_sp(parse_selector))),
-        |selectors| Selectors(selectors),
-    )(i)
-}
-
-/// 解析 选择器
-fn parse_selector(i: &str) -> IResult<&str, Selector> {
-    let (i, output) = is_not(",{")(i)?;
-    let mut selector = Vec::<Selector>::new();
-    for i in output.split_ascii_whitespace().into_iter() {
-        let (_, s) = alt((
-            parse_selector_id,
-            parse_selector_class,
-            parse_selector_tag,
-            parse_selector_all,
-        ))(i)?;
-        selector.push(s);
-    }
-    let selector = if selector.len() == 1 {
-        selector.pop().unwrap()
-    } else {
-        Selector::Combinator(selector)
-    };
-    Ok((i, selector))
-}
-
-/// 解析 Selector::Id
-fn parse_selector_id(i: &str) -> IResult<&str, Selector> {
-    map(preceded(tag("#"), is_not(",{")), |i: &str| {
-        Selector::Id(i.trim().into())
-    })(i)
-}
-
-/// 解析 Selector::Class
-fn parse_selector_class(i: &str) -> IResult<&str, Selector> {
-    map(preceded(tag("."), is_not(",{")), |i: &str| {
-        Selector::Class(i.trim().into())
-    })(i)
-}
-
-/// 解析 Selector::Tag
-fn parse_selector_tag(i: &str) -> IResult<&str, Selector> {
-    map(is_not(",{"), |i: &str| Selector::Tag(i.trim().into()))(i)
-}
-
-/// 解析 Selector::All
-fn parse_selector_all(i: &str) -> IResult<&str, Selector> {
-    map(tag("*"), |_| Selector::All)(i)
-}
-
-/// 解析 选择器列表
-fn parse_parameters(i: &str) -> IResult<&str, Properties> {
-    map(
-        many0(skip_sp(is_not_block_ending(parse_parameter))),
-        |properties| Properties(properties),
-    )(i)
-}
-
-fn parse_parameter(input: &str) -> IResult<&str, Property> {
-    Property::parse(input)
-}
-
-/*
-    utils
-*/
-
-fn parameter_block<'a, O, P>(parser: P) -> impl FnMut(&'a str) -> IResult<&'a str, O>
-where
-    P: NomParser<&'a str, O, nom::error::Error<&'a str>>,
-{
-    delimited(tag("{"), parser, tag("}"))
-}
-
-/// 判断 下一个字符 不是 { 和 }
-fn is_not_block_ending<'a, O, P>(parser: P) -> impl FnMut(&'a str) -> IResult<&'a str, O>
-where
-    P: NomParser<&'a str, O, nom::error::Error<&'a str>>,
-{
-    // 如果下一个字符 不是{} 任意的一个, 则执行 parser, 使用peek, 不会对 preceded 的 input 产生影响
-    preceded(peek(none_of("{}")), parser)
-}
-
-#[cfg(test)]
-mod test {
-    use crate::{
-        parse::parse_comment,
-        properties::{Display, Height, Property, Width},
-    };
-
-    use super::{
-        parse_block, parse_css, parse_entities, parse_selector, parse_selector_class,
-        parse_selector_id, parse_selector_tag, parse_selectors, CssEntities, CssEntity, Properties,
-        Selector, Selectors,
-    };
-
-    #[test]
-    fn test_selector() {
-        assert_eq!(
-            parse_selector_id("#hello"),
-            Ok(("", Selector::Id("hello".into())))
-        );
-
-        assert_eq!(
-            parse_selector_class(".hello"),
-            Ok(("", Selector::Class("hello".into())))
-        );
-
-        assert_eq!(
-            parse_selector_tag("div"),
-            Ok(("", Selector::Tag("div".into())))
-        );
-
-        assert_eq!(parse_selector("div"), Ok(("", Selector::Tag("div".into()))));
-
-        assert_eq!(
-            parse_selectors("div"),
-            Ok(("", Selectors(vec![Selector::Tag("div".into())])))
-        );
-    }
-
-    #[test]
-    fn test_comment() {
-        assert_eq!(
-            parse_comment("/* this is comment */"),
-            Ok(("", " this is comment "))
-        );
-    }
-
-    #[test]
-    fn test_block() {
-        assert_eq!(
-            parse_block("/* this is comment */ .user { width: 100%; }"),
-            Ok((
-                "",
-                CssEntity::Block {
-                    selectors: Selectors(vec![Selector::Class("user".into())]),
-                    properties: Properties(vec![Property::Width(Width(100.0))])
-                }
-            ))
-        );
-        assert_eq!(
-            parse_block("/* this is comment */ #user { width: 100%; }"),
-            Ok((
-                "",
-                CssEntity::Block {
-                    selectors: Selectors(vec![Selector::Id("user".into())]),
-                    properties: Properties(vec![Property::Width(Width(100.0))])
-                }
-            ))
-        );
-        assert_eq!(
-            parse_block("/* this is comment */ div { width: 100%; }"),
-            Ok((
-                "",
-                CssEntity::Block {
-                    selectors: Selectors(vec![Selector::Tag("div".into())]),
-                    properties: Properties(vec![Property::Width(Width(100.0))])
-                }
-            ))
-        );
-    }
-
-    #[test]
-    fn test_entities() {
-        assert_eq!(
-            parse_entities(
-                r#"
-                    /* this is comment */
-                    div {
-                        width: 100%;
-                    }
-                    
-                    /* this is comment */
-                    .user0 {
-                        width: 100%;
-                    }
-
-                    /* this is comment */
-                    #user0 {
-                        height: 100%;
-                    }
-                "#
-            ),
-            Ok((
-                "",
-                CssEntities(vec![
-                    CssEntity::Block {
-                        selectors: Selectors(vec![Selector::Tag("div".into())]),
-                        properties: Properties(vec![Property::Width(Width(100.0))])
-                    },
-                    CssEntity::Block {
-                        selectors: Selectors(vec![Selector::Class("user0".into())]),
-                        properties: Properties(vec![Property::Width(Width(100.0))])
-                    },
-                    CssEntity::Block {
-                        selectors: Selectors(vec![Selector::Id("user0".into())]),
-                        properties: Properties(vec![Property::Height(Height(100.0))])
-                    }
-                ])
-            ))
-        );
-        assert_eq!(
-            parse_block("/* this is comment */ #user { width: 100%; }"),
-            Ok((
-                "",
-                CssEntity::Block {
-                    selectors: Selectors(vec![Selector::Id("user".into())]),
-                    properties: Properties(vec![Property::Width(Width(100.0))])
-                }
-            ))
-        );
-        assert_eq!(
-            parse_block("/* this is comment */ div { width: 100%; }"),
-            Ok((
-                "",
-                CssEntity::Block {
-                    selectors: Selectors(vec![Selector::Tag("div".into())]),
-                    properties: Properties(vec![Property::Width(Width(100.0))])
-                }
-            ))
-        );
-    }
-
-    #[test]
-    fn test_css_file() {
-        let css_data = include_str!("./simple.css");
-        assert_eq!(
-            parse_css(css_data),
-            Ok((
-                "",
-                CssEntities(vec![
-                    CssEntity::Block {
-                        selectors: Selectors(vec![Selector::Id("content".into())]),
-                        properties: Properties(vec![
-                            Property::Display(Display(1.0)),
-                            Property::Height(Height(500.0)),
-                        ])
-                    },
-                    CssEntity::Block {
-                        selectors: Selectors(vec![Selector::Class("first".into())]),
-                        properties: Properties(vec![Property::Height(Height(50.0)),])
-                    },
-                    CssEntity::Block {
-                        selectors: Selectors(vec![
-                            Selector::Combinator(vec![
-                                Selector::Id("content".into()),
-                                Selector::Class("first".into())
-                            ],),
-                            Selector::Combinator(vec![
-                                Selector::Id("content".into()),
-                                Selector::Class("second".into())
-                            ])
-                        ]),
-                        properties: Properties(vec![Property::Height(Height(50.0))])
-                    }
-                ])
-            ))
-        );
-    }
+fn log_init() {
+    let mut builder = ConfigBuilder::new();
+    builder.set_time_offset_to_local().unwrap();
+    CombinedLogger::init(vec![
+        WriteLogger::new(
+            LevelFilter::Info,
+            builder.build(),
+            std::fs::File::create("build.log").unwrap(),
+        ),
+        TermLogger::new(
+            LevelFilter::Info,
+            builder.build(),
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+    ])
+    .unwrap();
 }
